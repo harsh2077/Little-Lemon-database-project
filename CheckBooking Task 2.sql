c26a2edb54be68bb82cfd56b05e9aad8eecd9305
harsh2077 <110387163+harsh2077@users.noreply.github.com>
2024-06-18 21:55:56 +0530

    Update CheckBooking Task 2.sql

--- CheckBooking Task 2.sql
+++ CheckBooking Task 2.sql
@@ -1,5 +1,5 @@
 CREATE PROCEDURE CheckBooking(IN BookingDate DATE, IN TableNumber INT)
-SELECT BookingDate, TableNumber
+SELECT BookingDate
 WHERE exists (SELECT * from Booking where Date = BookingDate)
 	
 
